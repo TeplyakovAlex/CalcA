bc8addb8c5d50679b516a1b177e3dd99c5a91227
Alexandr Teplyakov <Teplyakov.in@gmail.com>
2016-06-06 18:27:03 +0300

    error some data

--- src/CalcMain.java
+++ src/CalcMain.java
@@ -20,7 +20,50 @@ public class CalcMain {
     public void test(String a, String b, String c) throws InterruptedException {
         double actualResult = Double.parseDouble(a) + Double.parseDouble(b);
         double expectedResult = Double.parseDouble(c);
-        Assert.assertEquals(actualResult, expectedResult, "Addition method failed");
+        Assert.assertEquals(actualResult, expectedResult,0.02, "Addition method failed");
         Thread.sleep(1000);
     }
+    @DataProvider(name = "Sub", parallel = true)
+    public static Object[][] Authentication1() throws Exception {
+        Object[][] testObjArray = ExcelOut.getTableArray("..\\CalcA\\src\\Data.xls", 1);
+        return testObjArray;
+    }
+
+    @Test(dataProvider = "Sub")
+    public void test1(String a, String b, String c) throws InterruptedException {
+        double actualResult = Double.parseDouble(a) - Double.parseDouble(b);
+        double expectedResult = Double.parseDouble(c);
+        Assert.assertEquals(actualResult, expectedResult,0.2, "Sub method failed");
+        Thread.sleep(1000);
+    }
+
+    @DataProvider(name = "Mult", parallel = true)
+    public static Object[][] Authentication2() throws Exception {
+        Object[][] testObjArray = ExcelOut.getTableArray("..\\CalcA\\src\\Data.xls", 2);
+        return testObjArray;
+    }
+
+    @Test(dataProvider = "Milt")
+    public void test2(String a, String b, String c) throws InterruptedException {
+        double actualResult = Double.parseDouble(a) * Double.parseDouble(b);
+        double expectedResult = Double.parseDouble(c);
+        Assert.assertEquals(actualResult, expectedResult,0.2, "Mult method failed");
+        Thread.sleep(1000);
+    }
+
+    @DataProvider(name = "Div", parallel = true)
+    public static Object[][] Authentication3() throws Exception {
+        Object[][] testObjArray = ExcelOut.getTableArray("..\\CalcA\\src\\Data.xls", 3);
+        return testObjArray;
+    }
+
+    @Test(dataProvider = "Div")
+    public void test3(String a, String b, String c) throws InterruptedException {
+        double actualResult = Double.parseDouble(a) / Double.parseDouble(b);
+        double expectedResult = Double.parseDouble(c);
+        Assert.assertEquals(actualResult, expectedResult,0.2, "Mult method failed");
+        Thread.sleep(1000);
+    }
+
+
 }
