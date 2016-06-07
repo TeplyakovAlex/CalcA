4f72dd0106203d125b6a5176831f5e3a298a360d
Alexandr Teplyakov <Teplyakov.in@gmail.com>
2016-06-07 17:41:57 +0300

    Good

--- src/CalcMain.java
+++ src/CalcMain.java
@@ -9,27 +9,29 @@ import org.testng.annotations.Test;
  * Created by diz on 04.06.2016.
  */
 public class CalcMain {
-
-    @DataProvider(name = "Addition", parallel = true)
+    @DataProvider(name = "Addition")
+    //@DataProvider(name = "Addition", parallel = true)
     public static Object[][] Authentication() throws Exception {
         Object[][] testObjArray = ExcelOut.getTableArray("..\\CalcA\\src\\Data.xls", 0);
         return testObjArray;
     }
 
-    @Test(dataProvider = "Addition")
+    @Test(dataProvider = "Addition",groups = "smoke test" )
+    //@Test(dataProvider = "Addition",dependsOnMethods={"test1"},dependsOnGroups ={"smoke test"} )
     public void test(String a, String b, String c) throws InterruptedException {
         double actualResult = Double.parseDouble(a) + Double.parseDouble(b);
         double expectedResult = Double.parseDouble(c);
         Assert.assertEquals(actualResult, expectedResult,0.02, "Addition method failed");
         Thread.sleep(1000);
     }
-    @DataProvider(name = "Sub", parallel = true)
+    @DataProvider(name = "Sub")
     public static Object[][] Authentication1() throws Exception {
         Object[][] testObjArray = ExcelOut.getTableArray("..\\CalcA\\src\\Data.xls", 1);
         return testObjArray;
     }
 
-    @Test(dataProvider = "Sub")
+    @Test(dataProvider = "Sub",groups = "smoke test")
+   // @Test(dataProvider = "Sub",dependsOnMethods={"test2"})
     public void test1(String a, String b, String c) throws InterruptedException {
         double actualResult = Double.parseDouble(a) - Double.parseDouble(b);
         double expectedResult = Double.parseDouble(c);
@@ -37,13 +39,13 @@ public class CalcMain {
         Thread.sleep(1000);
     }
 
-    @DataProvider(name = "Mult", parallel = true)
+    @DataProvider(name = "Mult")
     public static Object[][] Authentication2() throws Exception {
         Object[][] testObjArray = ExcelOut.getTableArray("..\\CalcA\\src\\Data.xls", 2);
         return testObjArray;
     }
 
-    @Test(dataProvider = "Mult")
+    @Test(dataProvider = "Mult",groups = "smoke")
     public void test2(String a, String b, String c) throws InterruptedException {
         double actualResult = Double.parseDouble(a) * Double.parseDouble(b);
         double expectedResult = Double.parseDouble(c);
@@ -51,13 +53,14 @@ public class CalcMain {
         Thread.sleep(1000);
     }
 
-    @DataProvider(name = "Div", parallel = true)
+    @DataProvider(name = "Div")
     public static Object[][] Authentication3() throws Exception {
         Object[][] testObjArray = ExcelOut.getTableArray("..\\CalcA\\src\\Data.xls", 3);
         return testObjArray;
     }
 
-    @Test(dataProvider = "Div")
+    @Test(dataProvider = "Div",groups = "test")
+    //@Test(dataProvider = "Div",groups = "smoke test")
     public void test3(String a, String b, String c) throws InterruptedException {
         double actualResult = Double.parseDouble(a) / Double.parseDouble(b);
         double expectedResult = Double.parseDouble(c);
